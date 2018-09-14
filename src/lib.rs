bc70a061a2b22c3e53561f9ccb9f312cadb6f769
Kevin Phung <phung.kevin@gmail.com>
2018-09-13 23:34:11 -0600

    create RollResult struct and implement the InstanceOf trait

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ extern crate rand;
 
 use rand::{thread_rng, Rng};
 use rand::distributions::Uniform;
+use std::any::{Any, TypeId};
 
 #[derive(Debug, Copy, Clone)]
 pub enum Die {
@@ -54,10 +55,26 @@ impl PartialEq for Dice {
     }
 }
 
+struct RollResult {
+    die: Die,
+    rolls: Vec<i32>,
+    modifier: i32,
+    total: i32
+}
+
+trait InstanceOf
+where
+    Self: Any,
+{
+    fn instance_of<U: ?Sized + Any>(&self) -> bool {
+        TypeId::of::<Self>() == TypeId::of::<U>()
+    }
+}
+impl<RollResult: ?Sized + Any> InstanceOf for RollResult {}
+
 #[cfg(test)]
 mod tests {
-    use super::Dice;
-    use super::Die;
+    use super::*;
 
     #[test]
     fn it_creates_a_four_sided_die() {
@@ -98,5 +115,13 @@ mod tests {
             let roll = d4._roll();
             assert!(roll >= 1 && roll <= 4);
         }
+    }
+
+    #[test]
+    fn it_returns_RollResult() {
+        let d6 = Dice::new(Die::D6);
+        let result = d6.roll();
+
+        assert!(result.instance_of::<RollResult>());
     }
 }
