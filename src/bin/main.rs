ff2bcb19c45a1da06761c82778f7895c6499b994
Kevin Phung <phung.kevin@gmail.com>
2018-09-14 23:20:49 -0600

    test run the dice and logger module

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,12 @@
+extern crate rustydice;
+
+use rustydice::dice::{Die, Roll};
+use rustydice::logger;
+
 fn main() {
-    println!("Hello, world!");
+    let roll = Roll::new(Die::D8).number_of_rolls(2).modifier(3).roll();
+    println!("{}",  logger::build_log(roll));
+
+    let roll = Roll::new(Die::D4).number_of_rolls(4).roll();
+    println!("{}",  logger::build_log(roll));
 }
