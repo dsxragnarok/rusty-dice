7a939fa44a309def75c4965d2645d3607b928775
Kevin Phung <phung.kevin@gmail.com>
2018-09-15 16:12:51 -0600

    implement parsing the unit die

--- src/command.rs
+++ src/command.rs
@@ -1,15 +1,3 @@
-/// ```
-/// d4
-/// 2d6
-/// 4d10+1
-/// 1d12-4
-/// // split on 'd' | 'D'
-/// ['', '4']
-/// ['2', '6']
-/// ['4', '10+1']
-/// ['1', '12-4']
-/// ```
-
 use dice::Die;
 use dice::Die::*;
 
@@ -17,6 +5,31 @@ pub struct Command {
     pub number_of_rolls: u32,
     pub die: Die,
     pub modifier: i32,
+}
+
+pub fn parse(input_text: &str) -> Command {
+    let string_parts: Vec<&str> = input_text.split('d').collect();
+
+    let n = string_parts[0];
+    let d = string_parts[1];
+
+    let die = match d {
+        "2" => D2,
+        "4" => D4,
+        "6" => D6,
+        "8" => D8,
+        "10" => D10,
+        "12" => D12,
+        "20" => D20,
+        "100" => D100,
+        _ => panic!("Unable to match the die type!"),
+    };
+
+    Command {
+        die,
+        modifier: 0,
+        number_of_rolls: 1,
+    }
 }
 
 #[cfg(test)]
@@ -29,7 +42,7 @@ mod tests {
         let input_string = "d4";
         let cmd = parse(input_string);
 
-        assert_eq!(cmd.number_of_rolls, 0);
+        assert_eq!(cmd.number_of_rolls, 1);
         assert_eq!(cmd.modifier, 0);
         assert_eq!(cmd.die as u32, D4 as u32);
     }
@@ -39,7 +52,7 @@ mod tests {
         let input_string = "d6";
         let cmd = parse(input_string);
 
-        assert_eq!(cmd.number_of_rolls, 0);
+        assert_eq!(cmd.number_of_rolls, 1);
         assert_eq!(cmd.modifier, 0);
         assert_eq!(cmd.die as u32, D6 as u32);
     }
@@ -49,7 +62,7 @@ mod tests {
         let input_string = "d8";
         let cmd = parse(input_string);
 
-        assert_eq!(cmd.number_of_rolls, 0);
+        assert_eq!(cmd.number_of_rolls, 1);
         assert_eq!(cmd.modifier, 0);
         assert_eq!(cmd.die as u32, D8 as u32);
     }
@@ -59,7 +72,7 @@ mod tests {
         let input_string = "d10";
         let cmd = parse(input_string);
 
-        assert_eq!(cmd.number_of_rolls, 0);
+        assert_eq!(cmd.number_of_rolls, 1);
         assert_eq!(cmd.modifier, 0);
         assert_eq!(cmd.die as u32, D10 as u32);
     }
@@ -69,7 +82,7 @@ mod tests {
         let input_string = "d12";
         let cmd = parse(input_string);
 
-        assert_eq!(cmd.number_of_rolls, 0);
+        assert_eq!(cmd.number_of_rolls, 1);
         assert_eq!(cmd.modifier, 0);
         assert_eq!(cmd.die as u32, D12 as u32);
     }
@@ -79,7 +92,7 @@ mod tests {
         let input_string = "d20";
         let cmd = parse(input_string);
 
-        assert_eq!(cmd.number_of_rolls, 0);
+        assert_eq!(cmd.number_of_rolls, 1);
         assert_eq!(cmd.modifier, 0);
         assert_eq!(cmd.die as u32, D20 as u32);
     }
@@ -89,7 +102,7 @@ mod tests {
         let input_string = "d100";
         let cmd = parse(input_string);
 
-        assert_eq!(cmd.number_of_rolls, 0);
+        assert_eq!(cmd.number_of_rolls, 1);
         assert_eq!(cmd.modifier, 0);
         assert_eq!(cmd.die as u32, D100 as u32);
     }
