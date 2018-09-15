a70ff76c1b8abb40b3e62eac07f8286b782daa0f
Kevin Phung <phung.kevin@gmail.com>
2018-09-15 15:50:04 -0600

    set up initial tests and imports for command module

--- src/command.rs
+++ src/command.rs
@@ -10,24 +10,28 @@
 /// ['1', '12-4']
 /// ```
 
+use dice::Die;
+use dice::Die::*;
+
 pub struct Command {
     pub number_of_rolls: u32,
-    pub die: dice::Die,
+    pub die: Die,
     pub modifier: i32,
-};
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use dice::Die::*;
 
     #[test]
     fn it_parses_a_unit_d4_die() {
         let input_string = "d4";
         let cmd = parse(input_string);
 
-        expect_eq!(cmd.number_of_rolls, 0);
-        expect_eq!(cmd.modifier, 0);
-        expect_eq!(cmd.die as u32, dice::Die::D4 as u32);
+        assert_eq!(cmd.number_of_rolls, 0);
+        assert_eq!(cmd.modifier, 0);
+        assert_eq!(cmd.die as u32, D4 as u32);
     }
 
     #[test]
@@ -35,9 +39,9 @@ mod tests {
         let input_string = "d6";
         let cmd = parse(input_string);
 
-        expect_eq!(cmd.number_of_rolls, 0);
-        expect_eq!(cmd.modifier, 0);
-        expect_eq!(cmd.die as u32, dice::Die::D6 as u32);
+        assert_eq!(cmd.number_of_rolls, 0);
+        assert_eq!(cmd.modifier, 0);
+        assert_eq!(cmd.die as u32, D6 as u32);
     }
 
     #[test]
@@ -45,9 +49,9 @@ mod tests {
         let input_string = "d8";
         let cmd = parse(input_string);
 
-        expect_eq!(cmd.number_of_rolls, 0);
-        expect_eq!(cmd.modifier, 0);
-        expect_eq!(cmd.die as u32, dice::Die::D8 as u32);
+        assert_eq!(cmd.number_of_rolls, 0);
+        assert_eq!(cmd.modifier, 0);
+        assert_eq!(cmd.die as u32, D8 as u32);
     }
 
     #[test]
@@ -55,9 +59,9 @@ mod tests {
         let input_string = "d10";
         let cmd = parse(input_string);
 
-        expect_eq!(cmd.number_of_rolls, 0);
-        expect_eq!(cmd.modifier, 0);
-        expect_eq!(cmd.die as u32, dice::Die::D10 as u32);
+        assert_eq!(cmd.number_of_rolls, 0);
+        assert_eq!(cmd.modifier, 0);
+        assert_eq!(cmd.die as u32, D10 as u32);
     }
 
     #[test]
@@ -65,9 +69,9 @@ mod tests {
         let input_string = "d12";
         let cmd = parse(input_string);
 
-        expect_eq!(cmd.number_of_rolls, 0);
-        expect_eq!(cmd.modifier, 0);
-        expect_eq!(cmd.die as u32, dice::Die::D12 as u32);
+        assert_eq!(cmd.number_of_rolls, 0);
+        assert_eq!(cmd.modifier, 0);
+        assert_eq!(cmd.die as u32, D12 as u32);
     }
 
     #[test]
@@ -75,9 +79,9 @@ mod tests {
         let input_string = "d20";
         let cmd = parse(input_string);
 
-        expect_eq!(cmd.number_of_rolls, 0);
-        expect_eq!(cmd.modifier, 0);
-        expect_eq!(cmd.die as u32, dice::Die::D20 as u32);
+        assert_eq!(cmd.number_of_rolls, 0);
+        assert_eq!(cmd.modifier, 0);
+        assert_eq!(cmd.die as u32, D20 as u32);
     }
 
     #[test]
@@ -85,8 +89,8 @@ mod tests {
         let input_string = "d100";
         let cmd = parse(input_string);
 
-        expect_eq!(cmd.number_of_rolls, 0);
-        expect_eq!(cmd.modifier, 0);
-        expect_eq!(cmd.die as u32, dice::Die::D100 as u32);
+        assert_eq!(cmd.number_of_rolls, 0);
+        assert_eq!(cmd.modifier, 0);
+        assert_eq!(cmd.die as u32, D100 as u32);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ extern crate rand;
 
 pub mod dice;
 pub mod logger;
+pub mod command;
