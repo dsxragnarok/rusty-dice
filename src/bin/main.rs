3012667b5ecfde440e9b0d2577c1fe87d7c52f0c
Kevin Phung <phung.kevin@gmail.com>
2018-09-15 22:41:03 -0600

    test string find and with match

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,21 +1,13 @@
-extern crate rustydice;
-
-use rustydice::dice::{Die, Roll};
-use rustydice::logger;
-
 fn main() {
-    let roll = Roll::new(Die::D8).number_of_rolls(2).modifier(3).roll();
-    let rolls  = &roll.rolls;
-    println!("{}", logger::build_log(&roll));
-    println!(" >>> {:?}", rolls);
+    let s = "3d6";
 
-    let roll = Roll::new(Die::D4).number_of_rolls(4).roll();
-    let rolls  = &roll.rolls;
-    println!("{}", logger::build_log(&roll));
-    println!(" >>> {:?}", rolls);
+    let idx = match s.find('+') {
+        Some(i) => i,
+        None => match s.find('-') {
+            Some(i) => i,
+            None => 0
+        }
+    };
 
-
-    let d = "d4";
-    let d: Vec<&str> = d.split('d').collect();
-    println!("{:?}", d);
+    println!("idx = {}", idx);
 }
