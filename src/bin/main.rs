f9c9c91e50fff049c5413c7fcd45d3483ee75acf
Kevin Phung <phung.kevin@gmail.com>
2018-09-16 14:32:06 -0600

    initial implementation of commandline interface

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,13 +1,34 @@
-fn main() {
-    let s = "3d6";
+extern crate rustydice;
 
-    let idx = match s.find('+') {
-        Some(i) => i,
-        None => match s.find('-') {
-            Some(i) => i,
-            None => 0
-        }
+use std::env;
+use std::process;
+use rustydice::command;
+use rustydice::dice;
+use rustydice::logger;
+
+static USAGE: &str = "USAGE:\trusty-dice ndx[+|-]m";
+
+fn main() {
+    let mut args = env::args();
+    args.next();
+
+    let query = match args.next() {
+        Some(arg) => arg,
+        None => {
+            println!("{}", USAGE);
+            process::exit(1);
+        },
     };
 
-    println!("idx = {}", idx);
+    let cmd = command::parse(&query[..]);
+
+    // need to implement a run() from Command struct
+    let roll = dice::Roll::new(cmd.die)
+                    .number_of_rolls(cmd.number_of_rolls)
+                    .modifier(cmd.modifier)
+                    .roll();
+
+    let log = logger::build_log(&roll);
+    println!("{}", log);
+    println!(" >>> {:?}", &roll.rolls);
 }
